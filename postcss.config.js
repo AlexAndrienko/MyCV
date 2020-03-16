1e1ed36457e23e232b7be9be6065dad30e2bdb77
AlexAndrienko <alex.ddevil.andrienko@gmail.com>
2020-03-17 00:56:56 +0200

    its alive

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   parser: 'postcss-scss',
-    plugins: [
-      require('autoprefixer')(),
-      require('postcss-mixins')(),
-      require('postcss-simple-vars')(),
-      require('postcss-extend')(),
-      require('postcss-nested')(),
-      require('postcss-inline-svg')(),
-      require('postcss-assets')({
-        basePath: 'images/'
-      }),
-      require('postcss-url')({ url: 'copy', useHash: true })
-    ]
-  }
+  plugins: [
+    require('autoprefixer')(),
+    require('postcss-mixins')(),
+    require('postcss-simple-vars')(),
+    require('postcss-extend')(),
+    require('postcss-nested')(),
+    require('postcss-inline-svg')(),
+    require('postcss-assets')({
+      basePath: 'images/'
+    }),
+    require('postcss-url')({ url: 'copy', useHash: true })
+  ]
+}
